c4ba6283d86ff4a7457f6a3e372a070228823ea7
yash25566 <yash25566@iiitd.ac.in>
2026-04-10 17:08:47 +0530

    Refactor conditions and formatting in simulator.py
    Refactor code for consistency and readability by adding parentheses to conditions and improving formatting.

--- simulator.py
+++ simulator.py
@@ -14,13 +14,13 @@ class RISCVSimulator:
 # converts smaller signed no. into 32 bit signed no.
     def signExtending(self,value,bits): 
         signedBit = 2 ** (bits-1)
-        if value & signedBit: # checking if no. is negative
+        if(value & signedBit): # checking if no. is negative
             value = value - (2 ** bits)
         return value
 
 # converts a 32 bit number into signed representation
     def signed_32(self,value): 
-        if value & 0x80000000:
+        if(value & 0x80000000):
             return value-0x100000000
         return value
 
@@ -34,8 +34,8 @@ class RISCVSimulator:
             with open(filename,'r') as f:
                 for line in f:
                     line=line.strip() # removes spaces and newlines
-                    if line:
-                        if len(line)!=32:
+                    if(line):
+                        if(len(line)!=32):
                             raise SystemExit(f"Error: invalid instruction length: '{line}'")
                         self.program_memory.append(line)
         except FileNotFoundError:
@@ -44,12 +44,12 @@ class RISCVSimulator:
 # converts binary string to integers and extracts instructions using bit masking
     def instructionDecoding(self,instruction): #breaks instructions into fields
         instr=int(instruction,2)
-        opcode = instr & 0x7F
-        rd = (instr>> 7) & 0x1F
-        funct3 = (instr>>12) & 0x07
-        rs1 = (instr>>15) & 0x1F
-        rs2 = (instr>>20) & 0x1F
-        funct7 = (instr>>25) & 0x7F
+        opcode=instr & 0x7F
+        rd=(instr>> 7) & 0x1F
+        funct3=(instr>>12) & 0x07
+        rs1=(instr>>15) & 0x1F
+        rs2=(instr>>20) & 0x1F
+        funct7=(instr>>25) & 0x7F
         return {
             'instr': instr,
             'opcode': opcode,
@@ -63,7 +63,7 @@ class RISCVSimulator:
 # Ensures memory operations are valid and safe
     def checkMem(self,addr,op): # validate memory access
         addr=self.unsigned_32(addr)
-        if addr%4 != 0:
+        if(addr%4 != 0):
             sys.stderr.write(f"Error: unaligned memory {op} at address 0x{addr:08X} (PC=0x{self.pc:08X})\n")
             self.terminate()
         memData=0x00010000<=addr<=0x0001007C
@@ -80,17 +80,17 @@ class RISCVSimulator:
 # Loads a 32 bit word from memory
     def lw(self,addr):
         addr=self.unsigned_32(addr)
-        if 0x00010000<=addr<=0x0001007C:
+        if(0x00010000<=addr<=0x0001007C):
             return self.data_memory[(addr-0x00010000)//4]
-        if 0x00000100<=addr<=0x0000017F:
+        if(0x00000100<=addr<=0x0000017F):
             return self.stack_memory[(addr-0x00000100)//4]
         return 0
     def sw(self,addr,value):
         addr =self.unsigned_32(addr) #making addr 32 bit
         value=self.unsigned_32(value) #making value 32 bit 
-        if 0x00010000<=addr<=0x0001007C: # checking if address within data memory range 
+        if(0x00010000<=addr<=0x0001007C): # checking if address within data memory range 
             self.data_memory[(addr-0x00010000)//4]=value
-        elif 0x00000100<=addr<=0x0000017F: # checking if address within stack memory range 
+        elif(0x00000100<=addr<=0x0000017F): # checking if address within stack memory range 
             self.stack_memory[(addr-0x00000100)//4]=value
 
     def execute_r_type(self,decoded):
@@ -98,157 +98,189 @@ class RISCVSimulator:
         f3,f7=decoded['funct3'],decoded['funct7']
         v1=self.registers[rs1]
         v2=self.registers[rs2]
-        #operations 
-        if   f3==0x0 and f7==0x00: result=self.unsigned_32(v1+v2) #add
-        elif f3==0x0 and f7==0x20: result=self.unsigned_32(v1-v2) #sub 
-        elif f3==0x1 and f7==0x00: result=self.unsigned_32(v1<<(v2 & 0x1F)) #sll
-        elif f3==0x2 and f7==0x00: result=1 if self.signed_32(v1)<self.signed_32(v2) else 0 #slt
-        elif f3==0x3 and f7==0x00: result=1 if v1<v2 else 0 #sltu
-        elif f3==0x4 and f7==0x00: result=self.unsigned_32(v1 ^ v2) #xor
-        elif f3==0x5 and f7==0x00: result=v1>>(v2 & 0x1F) #srl
-        elif f3==0x5 and f7==0x20: result=self.unsigned_32(self.signed_32(v1)>>(v2 & 0x1F))#sra
-        elif f3==0x6 and f7==0x00: result=self.unsigned_32(v1 | v2) #or
-        elif f3==0x7 and f7==0x00: result=self.unsigned_32(v1 & v2) #and
+    #operations 
+        if(f3==0x0 and f7==0x00):
+            result=self.unsigned_32(v1+v2)          #add
+        elif(f3==0x0 and f7==0x20):
+            result=self.unsigned_32(v1-v2)          #sub 
+        elif(f3==0x1 and f7==0x00):
+            result=self.unsigned_32(v1<<(v2 & 0x1F))           #sll
+        elif(f3==0x2 and f7==0x00):
+            if(self.signed_32(v1)<self.signed_32(v2)):           #slt
+                result=1         
+            else:
+                result=0
+        elif(f3==0x3 and f7==0x00):
+            if(v1<v):           #sltu
+                result=1         
+            else:
+                result=0
+        elif(f3==0x4 and f7==0x00):
+            result=self.unsigned_32(v1 ^ v2)       #xor
+        elif(f3==0x5 and f7==0x00):
+            result=v1>>(v2 & 0x1F)        #srl
+        elif(f3==0x5 and f7==0x20):
+            result=self.unsigned_32(self.signed_32(v1)>>(v2 & 0x1F))         #sra
+        elif(f3==0x6 and f7==0x00):
+            result=self.unsigned_32(v1 | v2)         #or
+        elif(f3==0x7 and f7==0x00):
+            result=self.unsigned_32(v1 & v2)           #and
         else:
             raise SystemExit(f"Error: unknown R-type f3={f3} f7={f7} at PC=0x{self.pc:08X}")
         
-        if rd!=0:
+        if(rd!=0):
             self.registers[rd]=result
         self.pc+=4 #pc increment 
-
+        
+#this is function for I-type
     def execute_i_type(self,decoded):
         rs1,rd,f3=decoded['rs1'],decoded['rd'],decoded['funct3']
         instr=decoded['instr']
         imm=self.signExtending((instr>>20) & 0xFFF,12)  # extending the last 12 bits 
         v1=self.registers[rs1]
         opcode=decoded['opcode']
-        if opcode == 0x03:
-            if f3 == 0x2:                                   # LW
+        if(opcode==0x03):
+            if(f3==0x2):                                   # LW
                 addr = self.unsigned_32(v1 + imm)
-                self.checkMem(addr, 'load')
+                self.checkMem(addr,'load')
                 val = self.lw(addr)
-                if rd != 0:
-                    self.registers[rd] = val
+                if(rd != 0):
+                    self.registers[rd]=val
             else:
                 raise SystemExit(
                     f"Error: unsupported load funct3={f3} at PC=0x{self.pc:08X}"
                 )
 
-        elif opcode == 0x13:
-            shamt = (instr >> 20) & 0x1F   # used by shift instructions
-            f7    = (instr >> 25) & 0x7F
+        elif(opcode==0x13):
+            shamt=(instr>>20) & 0x1F   # used by shift instructions
+            f7=(instr>>25) & 0x7F
 
-            if   f3 == 0x0:                                 # ADDI
-                result = self.unsigned_32(v1 + imm)
-            elif f3 == 0x2:                                 # SLTI
-                result = 1 if self.signed_32(v1) < imm else 0
-            elif f3 == 0x3:                                 # SLTIU
-                result = 1 if v1 < self.unsigned_32(imm) else 0
-            elif f3 == 0x4:                                 # XORI
-                result = self.unsigned_32(v1 ^ imm)
-            elif f3 == 0x6:                                 # ORI
-                result = self.unsigned_32(v1 | imm)
-            elif f3 == 0x7:                                 # ANDI
-                result = self.unsigned_32(v1 & imm)
-            elif f3 == 0x1 and f7 == 0x00:                 # SLLI
-                result = self.unsigned_32(v1 << shamt)
-            elif f3 == 0x5 and f7 == 0x00:                 # SRLI
-                result = v1 >> shamt
-            elif f3 == 0x5 and f7 == 0x20:                 # SRAI
-                result = self.unsigned_32(self.signed_32(v1) >> shamt)
+            if(f3==0x0):                                 # ADDI
+                result=self.unsigned_32(v1 + imm)
+            elif(f3==0x2):                                 # SLTI
+                if(self.signed_32(v1)<imm):
+                    result=1
+                else:
+                    result=0
+            elif(f3==0x3):                                 # SLTIU
+                if(v1<self.unsigned_32(imm)):
+                    result=1
+                else:
+                    result=0
+            elif(f3==0x4):                                 # XORI
+                result=self.unsigned_32(v1^imm)
+            elif(f3==0x6):                                 # ORI
+                result=self.unsigned_32(v1|imm)
+            elif(f3==0x7):                                 # ANDI
+                result=self.unsigned_32(v1&imm)
+            elif(f3==0x1 and f7==0x00):                 # SLLI
+                result=self.unsigned_32(v1<<shamt)
+            elif(f3==0x5 and f7 == 0x00):                 # SRLI
+                result=v1>>shamt
+            elif(f3==0x5 and f7==0x20):                 # SRAI
+                result=self.unsigned_32(self.signed_32(v1)>>shamt)
             else:
                 raise SystemExit(
                     f"Error: unknown I-type arith funct3={f3} funct7={f7} at PC=0x{self.pc:08X}"
                 )
 
-            if rd != 0:
+            if(rd != 0):
                 self.registers[rd] = result
 
-        elif opcode==0x67:#jalr
-            if f3==0x0:
+        elif(opcode==0x67):#jalr
+            if(f3==0x0):
                 ret_addr=self.unsigned_32(self.pc+4)
                 target=self.unsigned_32(v1+imm) & 0xFFFFFFFE
-                if rd!=0:
+                if(rd!=0):
                     self.registers[rd]=ret_addr
                 self.pc=target
                 return
         else:
             raise SystemExit(f"Error: unknown I-type opcode 0x{opcode:02X} at PC=0x{self.pc:08X}")
         self.pc+=4
-#now s type
+#this is function for S-type
     def execute_s_type(self,decoded):
         rs1,rs2,f3=decoded['rs1'],decoded['rs2'],decoded['funct3']
         instr=decoded['instr']
-        imm_lo=(instr>> 7) & 0x1F
+        imm_lo=(instr>>7) & 0x1F
         imm_hi=(instr>>25) & 0x7F
         imm=self.signExtending((imm_hi<<5) | imm_lo,12)
         v1=self.registers[rs1]
         v2=self.registers[rs2]
-        if f3==0x2:
+        if(f3==0x2):
             addr=self.unsigned_32(v1+imm)
             self.checkMem(addr,'store')
             self.sw(addr,v2)
         else:
             raise SystemExit(f"Error: unknown S-type f3={f3} at PC=0x{self.pc:08X}")
         self.pc+=4
-#the branch type code
+#this is function for B-type
     def execute_b_type(self,decoded):
         rs1,rs2,f3=decoded['rs1'],decoded['rs2'],decoded['funct3']
         instr=decoded['instr']
-        imm_11  =(instr>> 7) & 0x1
-        imm_4_1 =(instr>> 8) & 0xF
+              # breaking down immediate
+        imm_11=(instr>>7) & 0x1
+        imm_4_1=(instr>>8) & 0xF
         imm_10_5=(instr>>25) & 0x3F
-        imm_12  =(instr>>31) & 0x1
+        imm_12=(instr>>31) & 0x1
         imm=self.signExtending(
-            (imm_12<<12) | (imm_11<<11) | (imm_10_5<<5) | (imm_4_1<<1),13
+            (imm_12<<12) | (imm_11<<11) | (imm_10_5<<5) | (imm_4_1<<1),13      # joining all parts to convert it into 13 bits
         )
         v1=self.registers[rs1]
         v2=self.registers[rs2]
-        if   f3==0x0: taken=(v1==v2)
-        elif f3==0x1: taken=(v1!=v2)
-        elif f3==0x4: taken=self.signed_32(v1)< self.signed_32(v2)
-        elif f3==0x5: taken=self.signed_32(v1)>=self.signed_32(v2)
-        elif f3==0x6: taken=v1<v2
-        elif f3==0x7: taken=v1>=v2
+                   # checking branch conditions wrt funct3(f3)
+        if(f3==0x0):
+            taken=(v1==v2)
+        elif(f3==0x1):
+            taken=(v1!=v2)
+        elif(f3==0x4):
+            taken=self.signed_32(v1)< self.signed_32(v2)
+        elif(f3==0x5):
+            taken=self.signed_32(v1)>=self.signed_32(v2)
+        elif(f3==0x6):
+            taken=v1<v2
+        elif(f3==0x7):
+            taken=v1>=v2
         else:
-            raise SystemExit(f"Error: unknown B-type f3={f3} at PC=0x{self.pc:08X}")
-        if taken:
-            self.pc=self.unsigned_32(self.pc+imm)
+            raise SystemExit(f"Error: unknown B-type f3={f3} at PC=0x{self.pc:08X}")       # unknown instruction error in this B-type
+        if(taken): 
+            self.pc=self.unsigned_32(self.pc+imm)      # means branch is taken
         else:
-            self.pc+=4
-#u tyoe wala code
+            self.pc+=4          # means condition not satisfied therefor, branch is not taken
+#this is function for U-type
     def execute_u_type(self,decoded):
         rd=decoded['rd']
         instr=decoded['instr']
-        imm=instr & 0xFFFFF000
-        if decoded['opcode']==0x37:
-            if rd!=0:
+        imm=instr & 0xFFFFF000      # to get upper 20 bits
+        if(decoded['opcode']==0x37):
+            if(rd!=0):
                 self.registers[rd]=imm
-        elif decoded['opcode']==0x17:
-            if rd!=0:
+        elif(decoded['opcode']==0x17):
+            if(rd!=0):
                 self.registers[rd]=self.unsigned_32(self.pc+imm)
         else:
-            raise SystemExit(f"Error: unknown U-type opcode at PC=0x{self.pc:08X}")
+            raise SystemExit(f"Error: unknown U-type opcode at PC=0x{self.pc:08X}")       # unknown instruction error in U-type
         self.pc+=4
-#the j type
+#this is function for J-type
     def execute_j_type(self,decoded):
         rd=decoded['rd']
         instr=decoded['instr']
+             # to get immediate at those bit places
         imm_19_12=(instr>>12) & 0xFF
-        imm_11   =(instr>>20) & 0x1
+        imm_11 =(instr>>20) & 0x1
         imm_10_1 =(instr>>21) & 0x3FF
-        imm_20   =(instr>>31) & 0x1
+        imm_20 =(instr>>31) & 0x1
         imm=self.signExtending((imm_20<<20) | (imm_19_12<<12) | (imm_11<<11) | (imm_10_1<<1),21)
-        if rd!=0:
-            self.registers[rd]=self.unsigned_32(self.pc+4)
-        self.pc=self.unsigned_32(self.pc+imm)
+        if(rd!=0):
+            self.registers[rd]=self.unsigned_32(self.pc+4)       # to save return address in rd
+        self.pc=self.unsigned_32(self.pc+imm)        # to jump to desired address
 
 #now starting the virtual halt function 
     def is_virtual_halt(self,instruction): 
         d=self.instructionDecoding(instruction)
-        if d['opcode']!=0x63 or d['funct3']!=0x0:
+        if(d['opcode']!=0x63 or d['funct3']!=0x0):
             return False
-        if d['rs1']!=0 or d['rs2']!=0:
+        if(d['rs1']!=0 or d['rs2']!=0):
             return False
         n=d['instr']
         return (((n>> 7) & 0x01)==0 and 
@@ -259,7 +291,10 @@ class RISCVSimulator:
     def record_state(self):
         parts=[f"0b{self.pc:032b}"]
         for i in range(32):
-            val=0 if i==0 else self.registers[i]
+            if(i==0):
+                val=0
+            else:
+                val=self.registers[i]
             parts.append(f"0b{val:032b}")
         self.trace.append(" ".join(parts)+" ")
 
@@ -275,24 +310,30 @@ class RISCVSimulator:
         #now onto the hardware part
         for _ in range(10_000_000):
             instr_index=self.pc//4
-            if instr_index>=len(self.program_memory):
+            if(instr_index>=len(self.program_memory)):
                 sys.stderr.write(f"Error: PC 0x{self.pc:08X} is outside program memory.\n")
                 self.terminate()
             instruction=self.program_memory[instr_index]
-            if self.is_virtual_halt(instruction):
+            if(self.is_virtual_halt(instruction)):
                 self.record_state()
                 halted=True
                 break
             #binary to dictionary stuff
             decoded=self.instructionDecoding(instruction)
             opcode=decoded['opcode']
-            #cchecking konsa function hai opcode ka
-            if   opcode==0x33:               self.execute_r_type(decoded)
-            elif opcode in (0x03,0x13,0x67): self.execute_i_type(decoded)
-            elif opcode==0x23:               self.execute_s_type(decoded)
-            elif opcode==0x63:               self.execute_b_type(decoded)
-            elif opcode in (0x37,0x17):       self.execute_u_type(decoded)
-            elif opcode==0x6F:               self.execute_j_type(decoded)
+            #checking konsa function hai opcode ka
+            if(opcode==0x33):
+                self.execute_r_type(decoded)
+            elif(opcode in (0x03,0x13,0x67)): 
+                self.execute_i_type(decoded)
+            elif(opcode==0x23)):   
+                self.execute_s_type(decoded)
+            elif(opcode==0x63):   
+                self.execute_b_type(decoded)
+            elif(opcode in (0x37,0x17)):  
+                self.execute_u_type(decoded)
+            elif(opcode==0x6F):
+                self.execute_j_type(decoded)
             else:
                 sys.stderr.write(f"Error hai : unknown opcode 0b{opcode:07b} at PC=0x{self.pc:08X}\n")
                 self.terminate()
@@ -300,14 +341,17 @@ class RISCVSimulator:
             self.registers[0]=0
             self.record_state()
 
-        if halted:
+        if(halted):
             self.dump_memory()
 
     def write_trace(self,filename):
         try:
-            with open(filename,'w') as f:
-                for line in self.trace:
-                    f.write(line+'\n')
+            f=open(filename,'w')
+            lines=[]
+            for line in self.trace:
+                lines.append(line + '\n')
+            f.writelines(lines)
+            f.close()
         except IOError as e:
             raise SystemExit(f"Error writing trace to '{filename}': {e}")
 
@@ -317,5 +361,5 @@ def main():
     sim.execute(sys.argv[2])
     sim.write_trace(sys.argv[2])
 
-if __name__=="__main__":
+if(__name__=="__main__"):
     main()
